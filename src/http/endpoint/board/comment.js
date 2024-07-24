4586dce259042593bc5d70af813b98f2758990f0
beomshin.kim <chzh859@naver.com>
2024-07-24 23:32:50 +0900

    # border comment enroll 구현 - border comment enroll 구현 - 기존 로직 삭제

--- src/http/endpoint/board/comment.js
+++ src/http/endpoint/board/comment.js
@@ -2,8 +2,8 @@
 const END_POINT = {
     DELETE_ANONYMOUS_COMMENT_BOARD: '/api/public/board/delete/anonymous/comment', // 익명 게시판 댓글 삭제
     DELETE_USER_COMMENT_BOARD: '/api/board/delete/user/comment', // 회원 게시판 댓글 삭제
-    ENROLL_USER_COMMENT_BOARD: '/api/board/enroll/user/comment', // 회원 게시판 댓글 등록
-    ENROLL_ANONYMOUS_COMMENT_BOARD: '/api/public/board/enroll/anonymous/comment', // 익명 게시판 댓글 등록
+    ENROLL_USER_COMMENT_BOARD: '/api/v1/enroll/board/comment', // 회원 게시판 댓글 등록
+    ENROLL_ANONYMOUS_COMMENT_BOARD: '/api/public/v1/enroll/board/comment', // 익명 게시판 댓글 등록
     FIND_ANONYMOUS_ALL_COMMENT_BOARD: (id) => { return `/api/public/board/find/anonymous/all/comment/${id}` }, // 게시판 댓글 전체 조회(익명)
     FIND_USER_ALL_COMMENT_BOARD: (id) => { return `/api/board/find/user/all/comment/${id}` }, // 게시판 댓글 전체 조회(회원)
     FIND_ANONYMOUS_PARENT_COMMENT_BOARD: (id) => { return `/api/public/board/find/anonymous/parent/comment/${id}` }, // 게시판 부모 댓글 조회(익명)
