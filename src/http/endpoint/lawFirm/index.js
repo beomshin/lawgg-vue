a8351ec8b360b8356c1a43f3ca5476ecfe0264a3
beomshin.kim <chzh859@naver.com>
2024-07-26 19:39:13 +0900

    # 로펌 delete, cancel 작업 - 로펌 apply 구현 - 기존 로직 삭제

--- src/http/endpoint/lawFirm/index.js
+++ src/http/endpoint/lawFirm/index.js
@@ -4,7 +4,7 @@ const END_POINT = {
     CONFIRM_LAW_FIRM: '/api/law-firm/confirm', // 로펌 신청 접수하기
     ENROLL_LAW_FIRM: '/api/law-firm/enroll', // 로펌 만들기
     DELETE_LAW_FIRM: '/api/law-firm/delete', // 로펌 삭제하기
-    QUIT_LAW_FIRM: '/api/law-firm/quit', // 로펌 탈퇴하기
+    QUIT_LAW_FIRM: '/api/v1/quit/law-firm', // 로펌 탈퇴하기
     USER_DELETE_LAW_FIRM: '/api/law-firm/user/delete', // 로펌 회원 탈퇴시키기
     FIND_ALL_LAW_FIRM_LIST: '/api/public/law-firm/list', // 로펌 리스트 조회하기
     FIND_ANONYMOUS_LAW_FIRM_DETAIL: (id) => { return `/api/public/anonymous/law-firm/${id}` }, // 로펌 비회원 상세 조회하기
@@ -14,7 +14,7 @@ const END_POINT = {
     FIND_USER_LIST_MY_LAW_FIRM: '/api/law-firm/find/user/list/my', // 로펌 유저 리스트 조회하기
     ACTIVE_LAW_FIRM: '/api/law-firm/active', // 로펌 활성화&비활성화 하기
     UPDATE_LAW_FIRM: '/api/law-firm/update', // 로펌 수정하기
-    CANCEL_LAW_FIRM: '/api/law-firm/user/cancel' // 로펌 취소하기
+    CANCEL_LAW_FIRM: '/api/v1/cancel/apply/law-firm' // 로펌 취소하기
 }
 
 export default END_POINT
