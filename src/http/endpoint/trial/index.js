c0b779b0ee1a1993aa761644b580244ae9ac07f1
beomshin.kim <chzh859@naver.com>
2024-07-27 23:46:38 +0900

    # trial enroll 작업 - trial enroll 구현 - 기존 로직 삭제

--- src/http/endpoint/trial/index.js
+++ src/http/endpoint/trial/index.js
@@ -2,7 +2,7 @@
 const END_POINT = {
     LOGIN_USER_TRIAL: '/api/trial/login/user', // 트라이얼 로그인 (회원)
     DELETE_USER_TRIAL: '/api/trial/delete/user', // 트라이얼 게시판 삭제(회원)
-    ENROLL_USER_TRIAL: '/api/trial/enroll/user', // 회원 트라이얼 등록
+    ENROLL_USER_TRIAL: '/api/v1/enroll/trial', // 회원 트라이얼 등록
     ENROLL_LAW_FIRM_TRIAL: '/api/trial/enroll/law-firm', // 로펌 트라이얼 등록
     FIND_ALL_LIST_TRIAL: '/api/public/v1/find/trials', // 트라이얼 게시판 전체 조회
     FIND_USER_LIST_TRIAL: '/api/public/trial/find/user/list', // 회원 트라이얼 게시판 조회
@@ -16,7 +16,7 @@ const END_POINT = {
     UPDATE_TRIAL: '/api/trial/update',  // 트라이얼 수정
     UPDATE_LIVE_TRIAL: '/api/trial/update/live',  // 트라이얼 라이브
     UPDATE_END_TRIAL: '/api/trial/update/end', // 트라이얼 종료
-    UPLOAD_VIDEO_TRIAL: '/api/trial/enroll/video',
+    UPLOAD_VIDEO_TRIAL: '/api/v1/enroll/video',
 }
 
 export default END_POINT
