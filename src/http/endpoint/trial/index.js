b69202b488c312318d12aafbe2665d3b5d77c860
chzh859@naver.com <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>
2024-07-27 17:17:58 +0900

    # trial find 작업

--- src/http/endpoint/trial/index.js
+++ src/http/endpoint/trial/index.js
@@ -4,11 +4,11 @@ const END_POINT = {
     DELETE_USER_TRIAL: '/api/trial/delete/user', // 트라이얼 게시판 삭제(회원)
     ENROLL_USER_TRIAL: '/api/trial/enroll/user', // 회원 트라이얼 등록
     ENROLL_LAW_FIRM_TRIAL: '/api/trial/enroll/law-firm', // 로펌 트라이얼 등록
-    FIND_ALL_LIST_TRIAL: '/api/public/trial/find/all/list', // 트라이얼 게시판 전체 조회
+    FIND_ALL_LIST_TRIAL: '/api/public/v1/find/trials', // 트라이얼 게시판 전체 조회
     FIND_USER_LIST_TRIAL: '/api/public/trial/find/user/list', // 회원 트라이얼 게시판 조회
-    FIND_LAW_FIRM_LIST_TRIAL: '/api/public/law-firm/trial/find/all/list', // 로펌 트라이얼 게시판 전체 조회
-    FIND_ANONYMOUS_DETAIL_TRIAL: (id) => { return `/api/public/trial/find/anonymous/detail/${id}` },  // 트라이얼 게시판 상세 조회(익명)
-    FIND_USER_DETAIL_TRIAL: (id) => { return `/api/trial/find/user/detail/${id}` }, // 트라이얼 게시판 상세 조회(화원)
+    FIND_LAW_FIRM_LIST_TRIAL: '/api/public/v1/find/law-firm/trials', // 로펌 트라이얼 게시판 전체 조회
+    FIND_ANONYMOUS_DETAIL_TRIAL: (id) => { return `/api/public/v1/find/trial/${id}` },  // 트라이얼 게시판 상세 조회(익명)
+    FIND_USER_DETAIL_TRIAL: (id) => { return `/api/v1/find/trial/${id}` }, // 트라이얼 게시판 상세 조회(화원)
     RECOMMEND_TRIAL: '/api/trial/recommend', // 트라이얼 게시판 추천
     DELETE_RECOMMEND_TRIAL: '/api/trial/recommend/delete', // 트라이얼 추천 삭제
     REPORT_TRIAL: '/api/public/trial/report', // 트라이얼 신고
