a7d904a386152409c8c9099c74a14541f63adf34
beomshin.kim <chzh859@naver.com>
2024-08-01 20:27:15 +0900

    # user enroll 작업 - user enroll 작업 - 기존 로직 삭제

--- src/http/endpoint/sign/index.js
+++ src/http/endpoint/sign/index.js
@@ -3,7 +3,7 @@ const END_POINT = {
     CHECK_ID_SIGN: '/api/public/sign/check/id',
     SEND_EMAIL_SIGN: '/api/public/sign/send/email',
     VERIFY_EMAIL_SIGN: (txId) => { return `/api/public/sign/verify/email/${txId}` },
-    USER_SIGN: '/api/public/sign/user',
+    USER_SIGN: '/api/public/v1/enroll/user',
     CHECK_NICK_NAME: '/api/public/sign/check/nickName',
 }
 
